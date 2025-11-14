0bd051d448706ea23137da9eaf4e15535f0c1ced
Javon Harper <javon.d.harper@gmail.com>
2025-11-13 23:26:20 -0500

    add some placeholder commands

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,9 @@ fn main() {
         .bin_name("crate")
         .about("a music cli tool")
         .subcommand_required(true)
-        .subcommand(Command::new("init").about("initializes a new crate instance"))
-        .subcommand(Command::new("import").about("imports a new crate instance"));
+        .subcommand(Command::new("import").about("imports a new crate instance"))
+        .subcommand(Command::new("ls").about("search for music"))
+        .subcommand(Command::new("stats").about("get basic stats about your music collection"));
 
     let matches = cmd.get_matches();
 
@@ -33,6 +34,12 @@ fn main() {
         }
         Some(("import", _)) => {
             core::import();
+        }
+        Some(("stats", _)) => {
+            println!("Not implemented yet");
+        }
+        Some(("ls", _)) => {
+            println!("Not implemented yet");
         }
         _ => {
             println!("Unknown command");
